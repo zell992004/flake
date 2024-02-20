a85dfd8ff917596c98d8670a2bc55298abded70d
zell992004 <zell992004@gmail.com>
2024-02-20 13:58:00 -0500

    typo fix

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -18,7 +18,7 @@ P72 = nixpkgs.lib.nixosSystem {
   };
 
 T480 = nixpkgs.lib.nixosSystem {
-    specialArgs = { inherit self inputs};
+    specialArgs = { inherit self inputs;};
     modules = 
     [ (../../hosts/T480/default.nix)]
     ++ [(nixos-hardware.nixosModules.lenovo-thinkpad-t480)]
