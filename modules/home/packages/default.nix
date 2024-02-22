73c797c35cd9284b7c98394ef745321dbb38293b
zell992004 <zell992004@gmail.com>
2024-02-22 10:21:59 -0500

    add neofetch, and options for ressurect

--- modules/home/packages/default.nix
+++ modules/home/packages/default.nix
@@ -14,6 +14,7 @@
     config = mkIf cfg.enable {
         home.packages = with pkgs; [
       # others
+      neofetch
       openssl
       libnotify
       pamixer
--- modules/home/tmux/default.nix
+++ modules/home/tmux/default.nix
@@ -12,6 +12,7 @@ programs.tmux ={
   escapeTime = 10;
   plugins = (with pkgs; [
       tmuxPlugins.better-mouse-mode
+      tmuxPlugins.continuum
       tmuxPlugins.resurrect
       ]);
   extraConfig = ''
@@ -19,6 +20,8 @@ programs.tmux ={
   resurrect_dir="$HOME/.tmux/resurrect"
   set -g @resurrect-dir $resurrect_dir
   set -g @resurrect-hook-post-save-all 'target=$(readlink -f $resurrect_dir/last); sed "s| --cmd .*-vim-pack-dir||g; s|/etc/profiles/per-user/$USER/bin/||g; s|/home/$USER/.nix-profile/bin/||g" $target | sponge $target'
+  set -g @continuum-boot 'on'
+  set -g @continuum-boot-options 'foot'
   '';
 };
 };
