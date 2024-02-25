cf41a91052762d805a9baa9fd19be387e0ca872a
zell992004 <110198440+zell992004@users.noreply.github.com>
2024-02-25 06:22:00 -0800

    Update default.nix

--- modules/home/nixvim/default.nix
+++ modules/home/nixvim/default.nix
@@ -21,10 +21,74 @@ in {
       };
         plugins = {
             lightline.enable = true;
-         #   nvim-cmp = {
-         #       enable = true;
-         #       
-         #     };
+  "hrsh7th/nvim-cmp" = {
+      version = false;
+      event = "InsertEnter";
+      dependencies = [
+        "hrsh7th/cmp-nvim-lsp"
+        "hrsh7th/cmp-buffer"
+        "hrsh7th/cmp-path"
+      ];
+      opts = ''
+        vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+        local cmp = require("cmp")
+        local defaults = require("cmp.config.default")()
+        return {
+          completion = {
+            completeopt = "menu,menuone,noinsert",
+          },
+          mapping = cmp.mapping.preset.insert({
+            ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+            ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<C-e>"] = cmp.mapping.abort(),
+            ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            ["<S-CR>"] = cmp.mapping.confirm({
+              behavior = cmp.ConfirmBehavior.Replace,
+              select = true,
+            }),
+            ["<C-CR>"] = function(fallback)
+              cmp.abort()
+              fallback()
+            end,
+          }),
+          sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "path" },
+          }, {
+            { name = "buffer" },
+          }),
+          formatting = {
+            format = function(_, item)
+              local icons = require("lazyvim.config").icons.kinds
+              if icons[item.kind] then
+                item.kind = icons[item.kind] .. item.kind
+              end
+              return item
+            end,
+          },
+          experimental = {
+            ghost_text = {
+              hl_group = "CmpGhostText",
+            },
+          },
+          sorting = defaults.sorting,
+        }
+      '';
+      config = ''
+        ---@param opts cmp.ConfigSchema
+        config = function(_, opts)
+          for _, source in ipairs(opts.sources) do
+            source.group_index = source.group_index or 1
+          end
+          require("cmp").setup(opts)
+        end
+      '';
+    };
+  };
+}
          #     luasnip = {
          #       enable = true;
          #       extraConfig = { 
