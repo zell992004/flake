4da0a02b87770467f2e71a07490debb489af3675
zell992004 <zell992004@gmail.com>
2024-02-26 18:32:39 -0500

    1

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -20,7 +20,7 @@ P72 = nixpkgs.lib.nixosSystem {
 mserver = nixpkgs.lib.nixosSystem {
     specialArgs = { inherit self inputs; };
     modules = 
-    [ (../../hosts/mserver//default.nix)]
+    [ (../../hosts/mserver/default.nix)]
     ;
   };
 
