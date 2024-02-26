2b418e30ae3baf6cdd2b7c9da526fac0fbb9f75a
zell992004 <zell992004@gmail.com>
2024-02-26 18:20:03 -0500

    add new serv

---
+++ hosts/mserver/nix.nix
@@ -0,0 +1,24 @@
+{pkgs, inputs, config, nixpkgs, self, ...}: {
+
+  boot.loader grub = {
+      enable = true;
+      device = "/dev/sda";
+      useOSProber = true;
+    };
+  
+  networking = {
+    hostName = "mserver";
+    networkmanager.enable = true;
+    firewall.enable = false;
+    };
+
+  services.xserver = {
+    enable = true;
+    xkb.layout = "us";
+  #  videoDrivers = [ "nvidia" ];
+    displayManager.autoLogin = {
+      enable = true;
+      user = "zell";
+    }; 
+
+  
