acd5213bdcb083406bb16a38f7a9b3e7c0e534d1
zell992004 <zell992004@gmail.com>
2024-02-29 10:24:45 -0500

    monitor changes

--- hosts/P72/homemanager.nix
+++ hosts/P72/homemanager.nix
@@ -1,8 +1,9 @@
 {pkgs, inputs, config, nixpkgs, self, ...}: {
    home-manager.users.zell.wayland.windowManager.hyprland.settings.monitor = [
      "eDP-1,3840x2160,0x0,2"
-     "HDMI-A-1,1920x1080,1920x-1080, 1"
-     "DP-5,1920x1080,3840x-1080, 1"
-     "DP-4,1920x1080,0x-1080,1,transform,3"
+     "HDMI-A-1,1920x1080,1920x1080, 1"
+     "DP-5,1920x1080,3840x1080, 1"
+     "DP-4,1920x1080,0x1080,1,transform,3"
+     
       ];
 }
