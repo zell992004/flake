b515f555a5a5c4bd046b784fdaa9afe0169b1eaa
zell992004 <zell992004@gmail.com>
2024-02-29 09:28:00 -0500

    fix display

--- hosts/P72/homemanager.nix
+++ hosts/P72/homemanager.nix
@@ -1,8 +1,8 @@
 {pkgs, inputs, config, nixpkgs, self, ...}: {
    home-manager.users.zell.wayland.windowManager.hyprland.settings.monitor = [
      "eDP-1,3840x2160,0x0,2"
-     "HDMI-A-1,1920x1080,1920x0, 1"
-     "DP-5,1920x1080,3840x0, 1"
-     "DP-4,1920x1080,-1920x0,1"
+     "HDMI-A-1,1920x1080,1920x-1080, 1"
+     "DP-5,1920x1080,3840x-1080, 1"
+     "DP-4,1920x1080,0x-1080,1,transform,3"
       ];
 }
